184d85a7920a5217f87ff960784a181ab23832dd
Laura B <Laura B@Laura>
2017-02-05 12:39:45 +0100

    Treballant amb ordres Git i branques v0.1

--- src/prjava02.java
+++ src/prjava02.java
@@ -11,6 +11,7 @@ import java.net.*;
 public class prjava02 {
 public static void main(String[] args){
     System.out.println("versió 0.1 del projecte prjava02");
+	System.out.println("Creació d'una branca del projecte prjava02");
     try{
         InetAddress adreça =InetAddress.getLocalHost();
         String hostname=adreça.getHostName();
